f5435f1b5f794983143efd82d640c59bd317358d
Adam <b9670094@gmail.com>
2017-03-11 20:19:11 +0800

    c

--- src/com/spring/rest/v2/controller/BootstrapDemoController.java
+++ src/com/spring/rest/v2/controller/BootstrapDemoController.java
@@ -1,22 +1,11 @@
 package com.spring.rest.v2.controller;
 
-import java.io.File;
-import java.util.List;
-
 import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -24,14 +13,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.rest.v2.base.BaseController;
-import com.spring.rest.v2.model.User;
-import com.spring.rest.v2.service.UserService;
 import com.spring.rest.v2.utils.Constant;
-import com.spring.rest.v2.utils.Constant.Status;
-import com.spring.rest.v2.utils.ShareTool;
-import com.spring.rest.v2.vo.ResponseVO;
-import com.spring.rest.v2.vo.form.UserCreateVO;
-import com.spring.rest.v2.vo.form.UserUpdateVO;
 
 
 @RestController
